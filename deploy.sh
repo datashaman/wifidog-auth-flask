80f34b50b54a14046bede22bbfbcfde4df2c0e82
datashaman <marlinf@datashaman.com>
2017-11-24 20:05:59 +0200

    install ops requirements

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ ssh-keyscan -H auth.datashaman.com >> $HOME/.ssh/known_hosts
 chmod 600 $HOME/.ssh/known_hosts
 pip install dotenvy fabric jinja2
 git clone -q https://github.com/datashaman/wifidog-auth-ops.git .ops
-cd .ops && fab -H ubuntu@auth.datashaman.com deploy:${BRANCH}
+cd .ops && pip install -r requirements.txt && fab -H ubuntu@auth.datashaman.com deploy:${BRANCH}
