61ae65b5d72237cdcd1cf4130c482de82b8387dd
datashaman <marlinf@datashaman.com>
2017-12-04 18:25:54 +0200

    add full path

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl "http://localhost:${VIRTUAL_PORT}/?auth_token=${AUTH_TOKEN}" || exit 1
+/usr/bin/curl "http://localhost:${VIRTUAL_PORT}/?auth_token=${AUTH_TOKEN}" || exit 1
