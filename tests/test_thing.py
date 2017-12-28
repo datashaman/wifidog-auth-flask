35f852cd06b68d23151031aea952669067609afc
datashaman <marlinf@datashaman.com>
2017-12-28 09:17:39 +0200

    -

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,13 +1,20 @@
 from tests import TestCase
 
-from auth.models import Country
+from auth.models import Country, Currency
 from wtforms.ext.sqlalchemy.fields import identity_key
 
 
 class TestThing(TestCase):
-    def test_it(self):
+    def test_country(self):
         with self.app.test_request_context():
             c = Country.query.first()
+            actual = identity_key(instance=c)
+            print(actual)
+            self.assertEqual((type(c), (c.id,)), actual)
+
+    def test_currency(self):
+        with self.app.test_request_context():
+            c = Currency.query.first()
             actual = identity_key(instance=c)
             print(actual)
             self.assertEqual((type(c), (c.id,)), actual)
